aced3d446629706bad65b0183f41d5d2eb7926b7
Vivek18 <viveksaha096@gmail.com>
2025-10-23 03:12:58 +0530

    Updated middleware

--- middleware.js
+++ middleware.js
@@ -2,7 +2,8 @@ import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server'
 
 const isPublicRoute = createRouteMatcher([
   '/sign-in(.*)',
-  '/sign-up(.*)'
+  '/sign-up(.*)',
+  '/'
 ])
 
 export default clerkMiddleware(async (auth, req) => {
